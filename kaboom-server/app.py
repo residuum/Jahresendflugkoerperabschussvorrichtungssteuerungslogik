22aa6fe605bc64ac68e9f97f466bad05be17a3a8
Thomas Mayer <thomas@residuum.org>
2013-12-07 17:41:32 +0100

    - Switch to angularjs for web interface. - Auto-update pin stati.

--- kaboom-server/app.py
+++ kaboom-server/app.py
@@ -2,32 +2,58 @@
 
 import RPi.GPIO as GPIO
 import web
+import json
 from time import sleep
+
+class pin:
+	def __init__(self, no, status):
+		self.no = no
+		self.status = status
+#	def __repr__(self):
+#		return json.dumps(self.__dict__)
         
+def jdefault(o):
+	return o.__dict__
+
 urls = (
 	'/(.*)', 'index',
 )
 
 render = web.template.render('templates/')
 
-class index:        
+class index:
+	def __init__(self):
+		GPIO.setmode(GPIO.BOARD)
+		self.pins = [13, 11, 7, 15, 19]
+		for pin in self.pins:
+			GPIO.setup(pin, GPIO.OUT)
+	
 	def GET(self, page):
 		if page == '':
 			return render.index()
+		if page == 'status':
+			stati = []
+			for pinNo in self.pins:
+				GPIO.setup(pinNo, GPIO.IN)
+				status = pin(pinNo, GPIO.input(pinNo))
+				stati.append(status)
+			web.header('Content-Type', 'application/json')
+			return json.dumps(stati, default=jdefault)
 
 	def POST(self, page):
 		if page == 'shoot':
-			post_params = web.input()
-			pins = [7, 11, 13, 15, 19]
-			pin = int(post_params.pin)
-			time = int(post_params.time)
-			if pin in pins and time < 15:
-				GPIO.setmode(GPIO.BOARD)
+			params = json.loads(web.data())
+			pin = int(params['pin'])
+			time = int(params['time'])
+			if pin in self.pins and time < 25:
 				GPIO.setup(pin, GPIO.OUT)
 				GPIO.output(pin, True)
 				sleep(time)
+				GPIO.setup(pin, GPIO.OUT)
 				GPIO.output(pin, False)
-				return "pin {0} time {1}".format(pin, time)
+				web.header('Content-Type', 'application/json')
+				return json.dumps(params);
+			return ''
 
 if __name__ == "__main__":
 	app = web.application(urls, globals())
---
+++ kaboom-server/static/controller.js
@@ -0,0 +1,32 @@
+'use strict'
+
+var kaboomApp = angular.module('kaboomApp', []);
+
+kaboomApp.controller('kaboom', function($scope, $http, $interval) {
+	$scope.availTimes = [5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25];
+	$scope.time = 10;
+	$scope.pins = [];
+	$scope.getStati = function() {
+		$http.get('/status').success(function(data) {
+			$scope.pins = data;
+		});
+	};
+	$scope.shoot = function(pinNo) {
+		$http.post('/shoot', {
+			pin: pinNo, 
+			time: $scope.time
+		});
+		for(var i = 0; i < $scope.pins.length; i++) {
+			if ($scope.pins[i].no === pinNo) {
+				$scope.pins[i].status = false;
+				break;
+			}
+		}
+	};
+
+	$interval(function(){
+		$scope.getStati()
+	}, 2000, 0, true);
+	
+	$scope.getStati();
+});
